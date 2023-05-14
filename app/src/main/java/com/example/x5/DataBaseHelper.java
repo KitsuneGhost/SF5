63c9d3d3a78224cc8170de19024dd596bde5396b
KitsuneGhost <tors_2005@bk.ru>
2023-05-14 20:48:44 +0300

    fixed db queries

--- app/src/main/java/com/example/x5/DataBaseHelper.java
+++ app/src/main/java/com/example/x5/DataBaseHelper.java
@@ -1,74 +1,80 @@
 package com.example.x5;
 
-import android.annotation.SuppressLint;
 import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class DataBaseHelper {
     private Connection connection;
 
     private static final String TAG = "DBHelper"; // тег для LogCat
 
-    // парметры
-    private final String host = "10.0.2.2";
-    private final String database = "StudyFor5_DB";
-    private final int port = 5432;
-    private final String user = "postgres";
-    private final String pass = "i3J!PqPz";
+    // парметры сервера и бд (в качестве сервера я пока использую локальный)
+    private final String host = "10.0.2.2"; // localhost и мой IP не работают
+    private final String database = "StudyFor5_DB"; // имя бд
+    private final int port = 5432; // порт сервера
+    private final String user = "postgres"; // пользователь
+    private final String pass = "i3J!PqPz"; // пароль
 
     private String url = "jdbc:postgresql://%s:%d/%s"; // строка-шаблон, для создания ссылки
 
     public DataBaseHelper() {  // конструктор
         this.url = String.format(this.url, this.host, this.port, this.database); // формируем полную ссылку
-        connect();
+        connect(); // вызов метода connect(), он описан ниже
     }
 
     private void connect() {
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread(new Runnable() { //создаём экземпляр класса thread
             @Override
-            public void run()
-            {
-                try
-                {
-                    Class.forName("org.postgresql.Driver");
-                    connection = DriverManager.getConnection(url, user, pass);
+            public void run() {  // переопределяем run()
+                try {
+                    Class.forName("org.postgresql.Driver"); // регистрируем драйвер
+                    connection = DriverManager.getConnection(url, user, pass); // создаем подключение к бд
                     Log.i(TAG, "Connection succeed!");
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) { // ловим исключения
                     Log.e(TAG, "Connection Failed!");
                     e.printStackTrace();
                 }
             }
         });
 
-        thread.start();
+        thread.start(); // запускаем поток
         try {
-            thread.join();
-            Log.i(TAG, "Thread Succeed!");
+            thread.join(); // ждем смерти потока
+            Log.i(TAG, "Connection Thread Succeed!"); // сообщаем об успешном завершении
         }
         catch (Exception e) {
-            Log.e(TAG, "Thread Failed!");
+            Log.e(TAG, "Connection Thread Failed!"); // или о неуспешном завершении
             e.printStackTrace();
         }
     }
 
 
-    public void register(String login, int hashcoded_password) {
-        try
-        {
-            String query = "INSERT INTO accounts (login, hashcodedPassword) VALUES (%s, %d)";
-            query = String.format(query, login, hashcoded_password);
-            connection.createStatement().executeQuery(query);
-            Log.i(TAG, "Query S!");
-        }
-        catch (SQLException e) {
-            Log.e(TAG, "SQL Exception caused by register method");
+    public void register(String login, int password) {  // метод для регистрации пользователей
+        Thread thread = new Thread(new Runnable() { // создаем экземпляр
+            @Override
+            public void run() { // переопределяем метод
+                try {
+                    String query = "INSERT INTO users (id, login, password) VALUES (1, '"  + login
+                            + "', " + password + ")"; // SQL запрос в бд
+                    connection.createStatement().executeQuery(query); // выполняем запрос
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start(); // запуск потока
+
+        try { // отчет о потоке
+            thread.join();
+            Log.i(TAG, "Reg Thread Succeed!");
+        } catch (Exception e) {
+            Log.e(TAG, "Reg Thread failed!");
             e.printStackTrace();
         }
     }
-
 }
